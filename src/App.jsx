2c87277a79f6e9e82ff6ae6a7951fd0fd1da78af
Adam-Petrovic <adam.petrovic2005@gmail.com>
2025-04-27 03:24:01 -0400

    Change was not visible trying this one

--- src/App.jsx
+++ src/App.jsx
@@ -22,11 +22,11 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          This is a test!!
         </p>
       </div>
       <p className="read-the-docs">
-        Hello this is a test!
+        Click on the Vite and React logos to learn more
       </p>
     </>
   )
